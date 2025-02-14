0db5582b1cc1fd171fa416560eaae7a64758fed3
jesusrodenas <73253282+jesusrodenas@users.noreply.github.com>
2025-02-14 10:14:54 +0100

    Nuevos cambios
    14/02/2025 - v.0.0.0 - nombrecompañero - Subo comentario aclaratorio al código.

--- ModuloEntornosDesarrollo/src/HolaMundo.java
+++ ModuloEntornosDesarrollo/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("¡Hola, mundo!");
 		System.out.println("¡Hola, mundo. Nueva versión!");
+		// Este es un comentario añadido por mi compañero a nuestro código.
 	}
 
 }
